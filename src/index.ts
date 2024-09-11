eb71f459e90394b9fd85d4556a77072746247390
Osama36 <osamazulfiqar36@gmail.com>
2024-09-12 00:01:58 +0500

    search by id

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,45 @@ const resolvers = {
     games() {
       return db.games
     },
+    game(root, args) {
+      return db.games.find(game => game.id === args.id)
+    },
     authors() {
       return db.authors
+    },
+    author(root, args) {
+      return db.authors.find(author => author.id === args.id)
     },
     reviews() {
       return db.reviews
     },
-  }
+    review(root, args) {
+      return db.reviews.find(review => review.id === args.id)
+    }
+
+  },
+  Game: {
+    reviews(game) {
+      return db.reviews.filter(review => review.game_id === game.id)
+    },
+
+  },
+  Author: {
+    reviews(author, args) {
+
+      return db.reviews.filter(review => review.author_id === author.id)
+
+
+    },
+  },
+  Review: {
+    author(review) {
+      return db.authors.find(author => author.id === review.author_id)
+    },
+    game(review) {
+      return db.games.find(game => game.id === review.game_id)
+    }
+  },
 }
 
 const server = new ApolloServer({
--- src/schema.ts
+++ src/schema.ts
@@ -5,21 +5,28 @@ export const typeDefs = `#graphql
         description: String!
         price: Float!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review{
         id: ID!
         description: String!
         rating: Int!
+        author: Author
+        game: Game
     }
     type Author{
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     type Query {
         games: [Game]
-        reviews: [Review]
+        game(id: ID!): Game
+        reviews(verified: Boolean): [Review]
+        review(id: ID!): Review,
         authors: [Author]
+        author(id: ID!): Author
     }
 `
 
